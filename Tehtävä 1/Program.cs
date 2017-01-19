86cfc421db0e195438f7359b0e2302ffab9e3111
K8284 <K8284@OHJE-330-WS-23>
2017-01-19 15:00:44 +0200

    Tehtävä 1 valmis

---
+++ Tehtävä 1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtävä_1
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Anna numero 1-3.");
+
+            int result;
+
+            if (int.TryParse(Console.ReadLine(), out result))
+
+            {
+
+                if (result == 1)
+
+                    Console.WriteLine("Lukusi on yksi");
+
+                else if (result == 2)
+
+                    Console.WriteLine("Lukusi on kaksi");
+                else if (result == 3)
+
+                    Console.WriteLine("Lukusi on kolme ");
+                else
+
+                    Console.WriteLine("joku muu luku");
+                Console.ReadLine();
+
+            }
+
+
+
+        }
+
+    }
