2a9a7d6fb9e8b95b7c22a3363b8eab8d637cb73a
K8284 <K8284@OHJE-330-WS-23>
2017-01-19 14:58:50 +0200

    Tehtävä 2

---
+++ Tehtävä 2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtävä 2
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Anna pisteesi");
+
+            int result;
+
+            if (int.TryParse(Console.ReadLine(), out result))
+
+            {
+
+                if (result == 0 || result == 1)
+
+                    Console.WriteLine("Numerosi on 0");
+
+                else if (result == 2 || result == 3)
+
+                    Console.WriteLine("Numerosi on ");
+                else if (result == 4 || result == 5)
+
+                    Console.WriteLine("Numerosi on 2");
+
+                else if (result == 6 || result == 7)
+
+                    Console.WriteLine("Numerosi on 3");
+
+                else if (result == 8 || result == 9)
+
+                    Console.WriteLine("Numerosi on 4");
+
+                else if (result == 10 || result == 11 || result == 12)
+
+                    Console.WriteLine("Numerosi on 5");
+
+                else
+
+                    Console.WriteLine("Joku muu luku");
+                Console.ReadLine();
+
+            }
+
+
+        }
+    }
+
+}
