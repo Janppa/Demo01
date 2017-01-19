146630482eb64222168e84cb71842837c724bbb0
K8284 <K8284@OHJE-330-WS-23>
2017-01-19 15:25:56 +0200

    Tehtävä 5 kesken

---
+++ Tehtävä 5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtävä_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sekuntti, minuutti, tunti,sekuntti2;
+            Console.Write("Anna aika sekuntteina");
+            int.TryParse(Console.ReadLine(), out sekuntti);
+            Console.Write("Aika minuutteina "+(sekuntti / 3600));
+            tunti = sekuntti / 3600;//tunnit
+
+            Console.Write("Tunteja " + tunti);
+            minuutti = sekuntti / 60; //minuutit
+
+            Console.Write("Minuutteja " + minuutti);
+            sekuntti2 = sekuntti - (minuutti * 60); //sekuntit
+            Console.Write("Sekuntteja " + sekuntti);
+
+            Console.Write(tunti + minuutti + sekuntti);
+           
+
+
+        }
+    }
+}
