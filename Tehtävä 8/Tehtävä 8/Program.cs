5639e8cfb5c7e27e77efd09e2495e94d4ba92245
K8284 <K8284@OHJE-330-WS-22>
2017-01-26 13:24:06 +0200

    Tehtävä 8 valmis

---
+++ Tehtävä 8/Tehtävä 8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtävä_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,b,c;
+            
+            Console.Write("Anna 3 kokonaislukua: \n");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Anna toinen luku: ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("Anna kolmas luku: ");
+            c = int.Parse(Console.ReadLine());
+            Console.Write("Suurin luku = " + Math.Max(Math.Max(a, b), c));
+            
+
+
+        }
+    }
+}
