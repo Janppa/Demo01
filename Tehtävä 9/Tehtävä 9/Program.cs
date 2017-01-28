2faba0b13d983218fafd9f7d27138bde4a60675f
JanppaHOME <Janppa@DESKTOP-BKRTDCM>
2017-01-28 15:48:43 +0200

    Tehtävä 9 kesken

---
+++ Tehtävä 9/Tehtävä 9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtävä_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int /*summa*/  luku; //alku;
+                                //alku = 0 + summa;
+                                
+            Console.Write("Anna luku ");
+            do {
+                luku = int.Parse(Console.ReadLine());
+                
+
+                Console.Write("Anna luku ",+luku);
+            } while (luku != 0);
+            
+            Console.Write("xaxaxa"/*+summa*/);
+        }
+    }
+}
