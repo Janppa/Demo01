8f789072374f63f9ebb53043551bc71c1041651a
JanppaHOME <Janppa@DESKTOP-BKRTDCM>
2017-01-28 15:10:49 +0200

    Tehtävä 8 valmis

--- Tehtävä 8/Tehtävä 8/Program.cs
+++ Tehtävä 8/Tehtävä 8/Program.cs
@@ -4,24 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Tehtävä_6
+namespace Tehtävä_8
 {
     class Program
     {
         static void Main(string[] args)
         {
             int a,b,c;
-            
-            Console.Write("Anna 3 kokonaislukua: \n");
-            a = int.Parse(Console.ReadLine());
-            Console.Write("Anna toinen luku: ");
-            b = int.Parse(Console.ReadLine());
-            Console.Write("Anna kolmas luku: ");
-            c = int.Parse(Console.ReadLine());
-            Console.Write("Suurin luku = " + Math.Max(Math.Max(a, b), c));
-            
+             
+            {
+
+                Console.Write("Anna kolme kokonaislukua: \n");
+                a = int.Parse(Console.ReadLine());
+                if (a == 0) { 
+                    Console.Write("Kielletty numero");
+                    Console.Write("\nAnna toinen luku: ");
+                   
+                }
+                else {
+                    Console.Write("Annoit luvun " + a);
+                    Console.Write("\nAnna toinen luku: ");
+                }
+                
+                    b = int.Parse(Console.ReadLine());
+                if (b == 0)
+                {
+                    Console.Write("Kielletty numero");
+                    Console.Write("\nAnna kolmas luku: ");
+                }
+                else
+                {
+                    Console.Write("Annoit luvun " + b);
+                    Console.Write("\nAnna kolmas luku: ");
+                }
+
+                c = int.Parse(Console.ReadLine());
+                if (c == 0)
+                {
+                    Console.Write("Kielletty numero");
+                }
+                else
+                {
+                    Console.Write("Annoit luvun " + c);
+                }
+                    }
+                Console.Write("\nSuurin luku = " + Math.Max(Math.Max(a, b), c), "\n");
+            Console.WriteLine();
+            }
+        
+        
 
 
         }
     }
-}
